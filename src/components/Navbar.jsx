fabb2aaea22a8f6a82b418bf86a56960cac9fb33
JoshCodedit <joshua.dixoncms@gmail.com>
2025-01-15 11:18:18 +0000

    feat: video added to homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import logoFaders from '../assets/images/logos/faders.png';
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 right-0 p-6 w-full flex items-center">
+    <nav className="fixed top-0 right-0 p-6 w-full flex items-center z-50">
       <img src={logoFaders} alt="logo" className="w-12 h-12 ml-20" />
       <ul className="flex gap-10 justify-end container mx-auto mr-20 font-['Cairo Play']">
         <li>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,17 @@
+import BackgroundVideo from '../components/BackgroundVideo';
+import headerMp4 from '../assets/videos/siteHeader0001-1187.mp4';
+
 export default function HomePage() {
-  return <div></div>;
+  return (
+    <div className="relative min-h-[50vh]">
+      <BackgroundVideo videoSrc={headerMp4} />
+      <div className="relative z-10 flex items-center justify-center">
+        <img
+          src="path-to-your-image.png"
+          alt="Description"
+          className="w-auto h-auto" // Adjust size as needed
+        />
+      </div>
+    </div>
+  );
 }
